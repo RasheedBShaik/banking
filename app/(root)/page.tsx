070b34e67b990885bff1687850b0537d3e535724
rasheedbshaik <rasheedbasha.2004@gmail.com>
2025-12-15 22:49:43 +0530

    implement chart in home page ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
+
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
+
 import React from 'react'
 
 const Home = () => {
+    const loggedin={firstName:'john'}
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+        <HeaderBox
+        type='greeting'
+        title="Welcome"
+        user={loggedin?.firstName||"Guest"}
+        subtext="Access and manage your account and transactions efficiently"
+        />
+        <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        />
+        </header>
+      </div>
+    </section>
   )
 }
 
